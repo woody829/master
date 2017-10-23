a45eae048a03d9aee0042acf10e980ce9a25e015
woody <279907022@qq.com>
2017-10-23 16:54:43 +0800

    Rejected commit: Default

---
+++ src/main/java/com/shrbank/service/Test.java
@@ -0,0 +1,11 @@
+package com.shrbank.service;
+
+/**
+ * Created by woody on 2017/10/23.
+ * just a test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
+}
